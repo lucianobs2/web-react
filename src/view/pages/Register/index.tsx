b1489f73e68f5e5ea91f3a127c83ce61bf48efdc
lucianobs1 <lucianotetsu@gmail.com>
2025-03-28 12:27:39 -0300

    Views: create Register page

--- src/view/pages/Register/index.tsx
+++ src/view/pages/Register/index.tsx
@@ -1,7 +1,35 @@
+import { Link } from 'react-router-dom';
+import { Input } from '../../components/Input';
+import { Button } from '../../components/Button';
+
 export function Register() {
   return (
-    <div>
-      <h1>Register Page</h1>
-    </div>
+    <>
+      <header className="flex flex-col items-center gap-4 text-center">
+        <h1 className="text-2xl font-bold text-gray-900 tracking-[-1px]">
+          Crie sua conta
+        </h1>
+
+        <p className="space-x-2">
+          <span className="text-gray-700 tracking-[-0.5px]">
+            Já possui uma conta?
+          </span>
+          <Link
+            to="/login"
+            className="tracking-[-0.5px] font-medium text-teal-900"
+          >
+            Fazer login
+          </Link>
+        </p>
+      </header>
+
+      <form className="mt-[60px] flex flex-col gap-4">
+        <Input name="name" placeholder="Nome" />
+        <Input name="email" type="email" placeholder="E-mail" />
+        <Input name="password" type="password" placeholder="Senha" />
+
+        <Button className="mt-2">Criar conta</Button>
+      </form>
+    </>
   );
 }
